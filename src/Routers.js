b7b21377760bf89b7780fd461a05ccc313052a78
Rizky Ridho <interrizky@ymail.com>
2022-03-12 15:48:23 +0700

    add react app to repo

---
+++ src/Routers.js
@@ -0,0 +1,18 @@
+// import React, { Fragment } from 'react'
+import React from 'react'
+// import { Container, Row, Col } from 'react-bootstrap'
+// import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import UserReducer from './Redux/UserReducer'
+// import { connect } from 'react-redux'
+// import Cookies from 'universal-cookie'
+
+class Routers extends React.Component {
+     render() {
+          return() {
+
+          }
+     }
+}
+
+export default Routers
