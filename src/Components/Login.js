cc58f0fa3dd414e2952f4a6c5624ef6c378491bd
Rizky Ridho <interrizky@ymail.com>
2022-06-02 12:40:00 +0700

    revamp login responsive

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import Axios from 'axios';
-
-/* ICO */
 import { EyeOff } from 'react-feather'
-/* Image */
 import imgEjavec from '../Assets/Images/ejavec-logo.svg'
-/* SweetAlert2 */
 import Swal from 'sweetalert2'
-/* Universal Cookie */
 import Cookies from 'universal-cookie'
+
 const cookies = new Cookies()
 
 class Login extends React.Component {
@@ -155,50 +151,47 @@ class Login extends React.Component {
 
   render() {
     return(
-      <div className="row login-page d-flex align-items-center">
-        <div className="wrapper-login mx-auto" id="login" style={{
+      <div className="login-page" id="login"  style={{
           backgroundColor: 'white', 
           justifyContent: 'center', 
           alignItems: 'center', 
           textAlign: 'center', 
-          width: '500px', 
-          height: '575px',                 
+          width: '50%',
           borderRadius: '15px'
         }}>
-          <div className="wrapper-image">
-            <img src={ imgEjavec } alt="LogoLogin" style={{ width: '225px', height: '225px' }} />
-            <h4>L O G I N</h4>
+          <div className="wrapper-image my-3 mx-auto">
+            <img className="logoLogin w-50" alt="Logo Login EJAVEC" src={ imgEjavec } />
           </div>
-          <form>
-            <div className="wrapper-form mx-5" id="wrapper-form">
-              <div className="input-group mb-3" style={{ alignItems: "center" }}>
-                  <input autoFocus type="text" id="username" name="username" className="form-control" placeholder="Your Username" aria-label="Username" aria-describedby="basic-addon1" />
-              </div>
-              <div className="input-group mb-3" style={{ alignItems: "center" }}>
-                  <input type="password" id="passwd" name="passwd" className="form-control" placeholder="Your Password" aria-label="Password" aria-describedby="basic-addon2" autoComplete="off" />
-                  <div className="input-group-prepend">
-                    <div className="input-group-text"><EyeOff onClick={ this.showHideOne } /></div>
-                  </div>                  
-              </div>        
-              <div className="input-group mb-3" style={{ alignItems: "center" }}>
-                <button type="button" id="btnLogin" className="btn-danger form-control" onClick={ this.clickSubmit }>
-                  LOGIN
-                </button>        
-              </div>
-              <div className="input-group mb-3" style={{ alignItems: "center" }}>
-                <button type="button" id="btnRegister" className="btn-outline-danger form-control" onClick={ this.clickRegister }>
-                  NEW USERS? SIGN UP HERE
-                </button>        
-              </div>              
-              <div className="input-group" style={{ alignItems: "center", justifyContent: "center" }}>
-                <a href="/forgot" style={{ margin: "0", textDecoration: "none" }}>Forgot Password?</a>
-              </div>
-              <div className="input-group my-2" style={{ alignItems: "center", justifyContent: "center" }}>
-                <a href="/code" style={{ margin: "0", textDecoration: "none" }}>Resend The Registration Code</a>
-              </div>                
-            </div> 
-          </form>
-        </div>                 
+          <div className="wrapper-judul-login my-3 mx-auto">
+            <p className="login-text" style={{ fontSize: '20px', fontWeight: 'bold', fontStyle: 'italic' }}> L O G I N </p>
+          </div>
+          <div className="wrapper-form my-3 mx-4">
+            <div className="input-group mb-3 w-75 mx-auto" style={{ alignItems: "center" }}>
+                <input autoFocus type="text" id="username" name="username" className="form-control" placeholder="Your Username" aria-label="Username" aria-describedby="basic-addon1" />
+            </div>
+            <div className="input-group mb-3 w-75 mx-auto" style={{ alignItems: "center" }}>
+                <input type="password" id="passwd" name="passwd" className="form-control" placeholder="Your Password" aria-label="Password" aria-describedby="basic-addon2" autoComplete="off" />
+                <div className="input-group-prepend">
+                  <div className="input-group-text"><EyeOff onClick={ this.showHideOne } /></div>
+                </div>                  
+            </div>        
+            <div className="input-group mb-3 w-75 mx-auto" style={{ alignItems: "center" }}>
+              <button type="button" id="btnLogin" className="btn-danger form-control" onClick={ this.clickSubmit }>
+                LOGIN
+              </button>        
+            </div>
+            <div className="input-group mb-3 w-75 mx-auto" style={{ alignItems: "center" }}>
+              <button type="button" id="btnRegister" className="btn-outline-danger form-control" onClick={ this.clickRegister }>
+                SIGN UP HERE
+              </button>        
+            </div>              
+            <div className="input-group mb-3 w-75 mx-auto" style={{ alignItems: "center", justifyContent: "center" }}>
+              <a href="/forgot" style={{ margin: "0", textDecoration: "none" }}>Forgot Password?</a>
+            </div>
+            <div className="input-group mb-3 w-75 mx-auto" style={{ alignItems: "center", justifyContent: "center" }}>
+              <a href="/code" style={{ margin: "0", textDecoration: "none" }}>Resend The Registration Code</a>
+            </div>                
+          </div>    
       </div>     
     )
   }
